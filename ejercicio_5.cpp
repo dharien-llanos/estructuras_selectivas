f51936a71f7b248c883a411d8369df0695f6920b
dharien <dharien1317@gmail.com>
2025-06-02 15:24:55 -0500

    Update ejercicio_5.cpp
    ejercicio 4 de estructura selectivas:
    agrege Validación del sexo para aceptar mayúsculas y minúsculas.
    Validación básica de edad para que no sea negativa.

--- ejercicio_5.cpp
+++ ejercicio_5.cpp
@@ -1,31 +1,34 @@
-#include<bits/stdc++.h>
-	using namespace std;
-	int main()
-	
-	{
-		char sexo;
-		float num_pul;
-		int edad;
-		
-		cout<<"ingrese su sexo (F para femenino y M para masculino)"<<endl;
-		cin>>sexo;
-		
-		cout<<"ingrese su edad: "<<endl;
-		cin>>edad;
-		if(sexo=='f')
-		{
-			
-		
-		num_pul=(220-edad)/10.0;
-	}
-		else
-		{
-			num_pul=(210-edad)/10.0;
-		}
-		
-		cout<<"el numero de pulsaciones es: "<<num_pul<<endl;
-		return 0;
-		
-		
-		
-	}
+#include <iostream>
+using namespace std;
+
+int main(){
+    char sexo;
+    double num_pul;
+    int edad;
+
+    cout<<"Ingrese su sexo(F para femenino y M para masculino):";
+    cin>>sexo;
+    if(sexo=='f' || sexo=='F'){
+        cout<<"Ingrese su edad:";
+        cin>>edad;
+        if(edad<0){
+            cout<<"Edad invalida."<<endl;
+            return 1;
+        }
+        num_pul=(220-edad)/10.0;
+    } else if(sexo=='m' || sexo=='M'){
+        cout<<"Ingrese su edad:";
+        cin>>edad;
+        if(edad<0){
+            cout<<"Edad invalida."<<endl;
+            return 1;
+        }
+        num_pul=(210-edad)/10.0;
+    } else {
+        cout<<"Sexo invalido."<<endl;
+        return 1;
+    }
+
+    cout<<"El numero de pulsaciones es:"<<num_pul<<endl;
+    return 0;
+}
