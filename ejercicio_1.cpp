e953760da696b0da9a1a9775840a881d028e61bf
dharien <dharien1317@gmail.com>
2025-06-02 15:13:12 -0500

    Update ejercicio_1.cpp
    Mejoras en validación de entradas, precisión en cálculos

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -1,28 +1,32 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main()
-{
-	float c1,c2,c3;
-	float promedio;
-	cout<<"ingrese su primera calificacion: "<<endl;
-	cin>>c1;
-	cout<<"ingrese su segunda calificacion: "<<endl;
-	cin>>c2;
-	cout<<"ingrese su tercera calificacion: "<<endl;
-	cin>>c3;
-	
-	promedio=(c1+c2+c3)/3;
-	if(promedio>70)
-	{
-		
-		cout<<"el alumno aprueba el curso: "<<endl;
-		
-	}
-	else
-	{
-		cout<<"el alumno reprueba el curso "<<endl;
-		
-	}
-	
-	return 0;
+
+int main(){
+    double c1,c2,c3;
+    cout<<"Ingrese su primera calificacion(0-100):";
+    cin>>c1;
+    if(c1<0||c1>100){
+        cout<<"Calificacion invalida."<<endl;
+        return 1;
+    }
+    cout<<"Ingrese su segunda calificacion(0-100):";
+    cin>>c2;
+    if(c2<0||c2>100){
+        cout<<"Calificacion invalida."<<endl;
+        return 1;
+    }
+    cout<<"Ingrese su tercera calificacion(0-100):";
+    cin>>c3;
+    if(c3<0||c3>100){
+        cout<<"Calificacion invalida."<<endl;
+        return 1;
+    }
+    double promedio=(c1+c2+c3)/3.0;
+    cout<<"Promedio final:"<<promedio<<endl;
+    if(promedio>=70){
+        cout<<"El alumno aprueba el curso."<<endl;
+    }else{
+        cout<<"El alumno reprueba el curso."<<endl;
+    }
+    return 0;
 }
