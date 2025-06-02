906040391d63c79568279bde2628435a8b607bf3
dharien <dharien1317@gmail.com>
2025-06-02 15:25:57 -0500

    Update ejercicio_6.cpp
    ejercicio 6 de estructuas selectivas: Validacion de rangos para promedio (0-20) y pension positiva.

--- ejercicio_6.cpp
+++ ejercicio_6.cpp
@@ -1,27 +1,27 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main()
-{
-	float promedio;
-	float pension;
-	cout<<"ingrese el promedio del alumno: "<<endl;
-		cin>>promedio;
-	cout<<"ingrese el monto de la pension educativa: "<<endl;
-	cin>>pension;
-	
 
-	if(promedio>=18)
-	{
-		pension=pension-(pension*0.30);
-		
-	}
-	
-	else
-	{
-		pension=pension+(pension*0.18);
-		
-	}
-	
-	cout<<"el monto a pagar del alumno es: "<<pension<<" soles "<<endl;
-	return 0;
+int main(){
+    double promedio, pension;
+    cout<<"Ingrese el promedio del alumno:";
+    cin>>promedio;
+    if(promedio<0 || promedio>20){
+        cout<<"Promedio invalido."<<endl;
+        return 1;
+    }
+    cout<<"Ingrese el monto de la pension educativa:";
+    cin>>pension;
+    if(pension<0){
+        cout<<"Monto invalido."<<endl;
+        return 1;
+    }
+
+    if(promedio>=18){
+        pension=pension-(pension*0.30);
+    }else{
+        pension=pension+(pension*0.18);
+    }
+
+    cout<<"El monto a pagar del alumno es:"<<pension<<" soles"<<endl;
+    return 0;
 }
