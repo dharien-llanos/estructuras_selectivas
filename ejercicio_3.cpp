0f5ecefe3978ae3fb45186c291658a7cf36fde7f
dharien <dharien1317@gmail.com>
2025-06-02 15:22:08 -0500

    Update ejercicio_3.cpp
    ejercicio 3 de estructuras selectivas: Añadí validación para que precio y número de camisas no sean negativos.

--- ejercicio_3.cpp
+++ ejercicio_3.cpp
@@ -1,23 +1,26 @@
-#include<bits/stdc++.h>
-	using namespace std;
-	int main()
-	{
-		int nc;
-		float precio;
-		cout<<"ingrese el monto a pagar: "<<endl;
-		cin>>precio;
-		cout<<"ingrese el numero de camisas: "<<endl;
-		cin>>nc;
-		if(nc>=3)
-		{
-			precio=precio-(precio*0.20);
-			
-		}
-		else
-		{
-			precio=precio-(precio*0.10);
-			
-		}
-		cout<<"el pago total es: "<<precio<<" soles"<<endl;
-		return 0;
-	}
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nc;
+    double precio;
+    cout<<"Ingrese el monto a pagar:";
+    cin>>precio;
+    if(precio<0){
+        cout<<"Precio invalido. Debe ser positivo."<<endl;
+        return 1;
+    }
+    cout<<"Ingrese el numero de camisas:";
+    cin>>nc;
+    if(nc<0){
+        cout<<"Numero de camisas invalido. Debe ser positivo."<<endl;
+        return 1;
+    }
+    if(nc>=3){
+        precio=precio-(precio*0.20);
+    }else{
+        precio=precio-(precio*0.10);
+    }
+    cout<<"El pago total es:"<<precio<<" soles"<<endl;
+    return 0;
+}
